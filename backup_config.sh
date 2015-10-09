e205db73f3de92313104a2e150541ad8f81fa098
Nicolas Roos <nicolas.roos@serranetga.com>
2015-10-09 15:13:44 +0200

    backup all brews and global npms

--- backup_config.sh
+++ backup_config.sh
@@ -1,5 +1,12 @@
+#!/bin/zsh
 cp ~/.gitconfig .files/
 cp ~/.gitignore_global .files/
 cp ~/.zshrc .files/
 cp ~/.eslintrc .files/
 cp ~/.bash_profile .files/
+
+#Get all global node modules
+npm list -global -depth=0 -json > global_npm.json
+
+#Get all brew formulas
+brew list > brews_list.txt
